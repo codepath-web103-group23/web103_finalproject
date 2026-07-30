35531cec69a04ead839c21eeb19ba2c4d04799ea
WSL-lenovo-noahm <noahmckegney@gmail.com>
2026-07-30 12:51:54 -0700

    feat(client): add profile icon in nav & dropdown menu
    - profile image from github user account in nav
    - pressing on github icon shows a menu to navigate to profile or logout
    
    On branch profile-icon
    Changes to be committed:
    modified:   client/src/App.jsx
    new file:   client/src/assets/home-user.svg
    new file:   client/src/assets/log-out.svg
    modified:   client/src/components/Nav.jsx
    new file:   client/src/components/ProfileMenu.jsx

---
+++ client/src/components/ProfileMenu.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import logoutimg from '../assets/log-out.svg'
+import userimg from '../assets/home-user.svg'
+
+const ProfileMenu = ({ user }) => {
+
+  return (
+    <div style={styles.menuContainer}>
+      <div style={styles.profileInfo}>
+        <img
+          style={{
+            ...styles.profileImg,
+            backgroundImage: `url(${user.avatarurl})`
+          }}
+        />
+        <p>{user.username}</p>
+      </div>
+
+      <Link to="/profile" style={styles.btn}>
+        <img src={userimg}/>
+        <strong>Profile</strong>
+      </Link>
+      <Link to="/logout" style={styles.btn}>
+        <img src={logoutimg}/>
+        <strong>Logout</strong>
+      </Link>
+    </div>
+  )
+}
+
+export default ProfileMenu
+
+const styles = {
+  profileInfo: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  menuContainer: {
+    position: 'absolute',
+    top: '55px',
+    right: '12px',
+    zIndex: '1000',
+    border: 'solid black',
+    borderRadius: '10px',
+    borderWidth: '1px',
+    backgroundColor: 'white',
+    height: '230px',
+    width: '150px',
+  },
+  profileImg: {
+    // backgroundColor: "#27F561",
+    // backgroundImage: `url(${user.avatarurl})`,
+    // cursor: 'pointer',
+    display: 'block',
+    width: '4px',
+    height: '4px',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    fontSize: '15px',
+    color: 'black',
+    border: 'solid gray',
+    padding: '20px',
+    borderRadius: '50px',
+    borderWidth: '0.1px',
+    textDecoration: 'none',
+    margin: '10px',
+  },
+  btn: {
+    // backgroundColor: "#27F561",
+    // display: 'block',
+    display: 'flex',
+    alignItems: 'center',
+    gap: 5,
+    fontSize: '15px',
+    color: 'black',
+    border: 'solid black',
+    padding: '20px',
+    borderRadius: '5px',
+    borderWidth: '1px',
+    textDecoration: 'none',
+    margin: '10px',
+  },
+
+}
