c5649a054dabc9d1182413b9e6b5609dab53bce5
WSL-lenovo-noahm <noahmckegney@gmail.com>
2026-08-02 19:38:50 -0700

    fix(server): try '/{*splat}',
    Changes to be committed:
    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ app.use('/auth', authroutes)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
